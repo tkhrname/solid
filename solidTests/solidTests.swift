7990ad938ae9b7271f95188c979bb35894626a1c
Takehiro Watanabe <takehiro.watanabe.224@gmail.com>
2019-05-30 15:53:28 +0900

    Initial Commit

---
+++ solidTests/solidTests.swift
@@ -0,0 +1,34 @@
+//
+//  solidTests.swift
+//  solidTests
+//
+//  Created by 渡邊丈洋 on 2019/05/30.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import XCTest
+@testable import solid
+
+class solidTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
